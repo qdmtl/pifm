e2dfc2cc5e96eb5432896be438d5390e8032f14b
davvalent <d@ntnlv.ca>
2022-10-16 23:46:42 -0400

    Refactorisation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 /**
- * Plan d'expropriation et photographies du Faubourg à m'lasse
+ * Le Faubourg à m’lasse en ligne (FML)
+ * 
+ * Plan interactif du Faubourg à m’lasse (secteur Radio-Canada) intégrant des données
+ * structurées, des photograpies d’archives, des cartes historiques et d’autres données.
+ *
+ * @requires module:leaflet
+ * @author David Valentine <d@ntnlv.ca>
  */
+
+/**
+ * Mapbox config
+ * @todo encode access token
+ */ 
+const mapBoxURL = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+mapBoxConfig = {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>, Archives de Montréal",
+  id: "mapbox/streets-v11",
+  accessToken: "pk.eyJ1IjoiZGF2dmFsZW50IiwiYSI6ImNrdmluZjh6cTBnYjkydXFna3lnOWRwM3oifQ.7qwZCUJ2JW2WFJ8JtDQfUg"
+};
 
 (async () => {
 
@@ -8,50 +25,31 @@
    * Télécharger données (requête http)
    * Retourner application/geo+json content type
    * @todo voir https://geojson.org/geojson-ld/
+   * @todo voir linked places format
    */
   const response = await fetch("./buildings.json"),
   geojsonFeatures = await response.json(),
 
-  /**
-   * Mapbox tiles
-   * @todo encode access token
-   */ 
-  mapbox = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>, Archives de Montréal",
+  /** Base Layer */
+  baseLayer = L.tileLayer("https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors, Imagery © Google, Archives de Montréal",
     maxZoom: 20,
-    id: "mapbox/streets-v11",
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: "pk.eyJ1IjoiZGF2dmFsZW50IiwiYSI6ImNrdmluZjh6cTBnYjkydXFna3lnOWRwM3oifQ.7qwZCUJ2JW2WFJ8JtDQfUg"
+    tileSize: 256
   }),
 
-  /**
-   * Données de la couche du plan d'expropriation
-   * @todo Tuiles
-   */ 
-  imageUrl = "img/plan-fond-transparent-min.png",
-  imageBounds = [
-    [45.5155088875666, -73.55432183482827],
-    [45.52127103906887, -73.54793549518355]
-  ],
-
-  // Couche du plan
-  plan = L.imageOverlay(imageUrl, imageBounds, {
-    opacity: 0.7,
-    alt: "Plan d'expropriation du Faubourg à M'lasse"
+  /** Faubourg Layer */ 
+   faubourgLayer = L.tileLayer("https://ntnlv.ca/faubourg/tiles/{z}/{x}/{y}.png", {
+    maxZoom: 20,
+    tileSize: 256,
+    opacity: 1
   }),
 
-  // Instantiation de la carte
-  mlasse = L.map("map", {
-    layers: [mapbox, plan]
-  }).setView([45.51823567357893, -73.55085910368373], 18),
-
-  // Couche GeoJSON
+  /** GeoJSON Layer */
   geoJSON = L.geoJSON(geojsonFeatures, {
     onEachFeature: (feature, layer) => {
       if (feature.properties) {
         const informationsPopUp = `
-          <b>Informations</b>
+          <b>${feature.properties["appellation"]}</b>
           <ul>
             <li>${feature.properties["appellation"]}</li>
             <li>${feature.properties["rdfs:label"]}</li>
@@ -64,17 +62,40 @@
     }
   }),
 
-  // Contrôles
+  /** Controls */
   overlayMaps = {
-    "<span class=\"controles\">Plan d'expropriation</span>": plan,
-    "<span class=\"controles\">Institutions</span>": geoJSON/*,
-    "<span class=\"controles\">Utilisation du sol</span>": foo,
-    "<span class=\"controles\">Photographies aériennes des archives</span>": foo,
-    "<span class=\"controles\">Plan d'assurance</span>": foo,*/
-  };
+    "<span class=\"controles\">Plan d'expropriation</span>": faubourgLayer,
+    "<span class=\"controles\"></span>": geoJSON,
+  },
 
-  L.control.layers(null, overlayMaps).addTo(mlasse);
-  geoJSON.addTo(mlasse);
+  /**
+   * Overlay Map
+   * @deprecated
+   */
+  imageUrl = "img/plan-fond-transparent-min.png",
+  imageBounds = [
+    [45.5155088875666, -73.55432183482827],
+    [45.52127103906887, -73.54793549518355]
+  ],
+  plan = L.imageOverlay(imageUrl, imageBounds, {
+    opacity: 1,
+    alt: "Plan d'expropriation du Faubourg à M'lasse"
+  }),
+
+  /** Instantiation du plan */
+  planDuFaubourg = L.map("map", {
+    center: [45.51823567357893, -73.55085910368373],
+    zoom: 17.5,
+    minZoom: 15,
+    zoomDelta: 0.5,
+    layers: [
+      baseLayer,
+      faubourgLayer
+    ]
+  });
+
+  L.control.layers(null, overlayMaps).addTo(planDuFaubourg);
+  geoJSON.addTo(planDuFaubourg);
 
 })();
 
