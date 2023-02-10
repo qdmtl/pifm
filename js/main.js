74cbf49055b4fc3e5dc621f207d4295e8246ce79
davvalent <d@ntnlv.ca>
2023-02-10 17:07:18 -0500

    50/50 screen for popup information window

--- js/main.js
+++ js/main.js
@@ -29,71 +29,105 @@
     tilesUrl = tilesUrl.devUrl;
   }
 
-  /** Base Layer */
+  /** 
+   * Base Layer
+   * Photos aériennes 
+   */
   const baseLayer = L.tileLayer("https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
   attribution: "Map data &copy; QDMTL, Imagery © Google, Archives de Montréal",
     maxZoom: 20,
     tileSize: 256
   }),
 
-  /** FAML Layer */
-  faubourgLayer = L.tileLayer(tilesUrl, {
-    maxZoom: 20,
-    tileSize: 256,
-    opacity: 1
-  }),
+    /** FAML Layer */
+    faubourgLayer = L.tileLayer(tilesUrl, {
+      maxZoom: 20,
+      tileSize: 256,
+      opacity: 1
+    }),
 
-  /** GeoJSON Layer */
-  geoJSON = L.geoJSON(geojsonFeatures, {
-    onEachFeature: (feature, layer) => {
-      if (feature.properties) {
-        const informationsPopUp = `
-          <b>${feature.properties["appellation"]}</b>
-          <ul>
-            <li>${feature.properties["appellation"]}</li>
-            <li>${feature.properties["rdfs:label"]}</li>
-            <li>${feature.properties["qdmtl:thoroughfare"]}</li>
-            <li>${feature.properties["inventoryNumber"]}</li>
-          </ul>
-        `
-        layer.bindPopup(informationsPopUp);
+    /** Instanciation du plan */
+    planDuFaubourg = L.map("map", {
+      center: [45.51823567357893, -73.55085910368373],
+      zoom: 17.5,
+      minZoom: 15,
+      zoomDelta: 0.5,
+      zoomSnap: 0.5,
+      layers: [
+        baseLayer,
+        faubourgLayer
+      ]
+    }),
+
+    /**
+     * Créer un pane pour la fenêtre pop-up
+     * @todo lire doc pour la méthode createPane
+     * "Panes are DOM elements used to control the ordering of layers on the map."
+     * https://leafletjs.com/reference.html#map-pane
+     */
+    informationPane = planDuFaubourg.createPane('fixed', document.querySelector("#map")),
+
+    /** GeoJSON Layer */
+    geoJSON = L.geoJSON(geojsonFeatures, {
+      onEachFeature: (feature, layer) => {
+        if (feature.properties) {
+
+          const popUpInformation = `
+            <div class="classe-ajoutée-dans-le-but-de-placer-limage-pricipale">
+              <h1>${feature.properties["appellation"]}</h1>
+              <ul>
+                <li>${feature.properties["appellation"]}</li>
+                <li>${feature.properties["rdfs:label"]}</li>
+                <li>${feature.properties["qdmtl:thoroughfare"]}</li>
+                <li>${feature.properties["inventoryNumber"]}</li>
+              </ul>
+            </div>
+          `,
+
+            popup = L.popup({
+              pane: 'fixed',
+              className: 'popup-fixed',
+              autoPan: false
+            }).setContent(popUpInformation);
+
+          layer.bindPopup(popup);
+
+          layer.on("click", () => {
+            console.log("Message fired when clicking the geoJSON feature (the marker).");
+            
+            /**
+             * Trois prochaines instructions
+             * Déplacement et centrage de la punaise
+             */
+            let targetLatLng = L.latLng(
+              /** Coordonnées sont inversées avec GeoJSON */
+              feature.geometry.coordinates[1],
+              feature.geometry.coordinates[0],
+            );
+            const targetZoom = 20,
+              popUpWidth = document.querySelector(".leaflet-popup").clientWidth,
+              targetPoint = planDuFaubourg.project(targetLatLng, targetZoom).subtract([popUpWidth / 2, 0]);
+            targetLatLng = planDuFaubourg.unproject(targetPoint, targetZoom);
+            planDuFaubourg.setView(targetLatLng, targetZoom);
+          })
+        }
       }
+    }),
+
+    /** Controls */
+    overlayMaps = {
+      "<span class=\"controles\">Plan d'expropriation</span>": faubourgLayer,
+      "<span class=\"controles\">Bâtiments</span>": geoJSON,
+    },
+
+    addControls = L.control.layers(null, overlayMaps).addTo(planDuFaubourg),
+    addJsonLayer = geoJSON.addTo(planDuFaubourg);
+
+  planDuFaubourg.on("popupopen", () => {
+      console.log("Message fired on pop-up opening");
     }
-  }),
+  );
 
-  /** Controls */
-  overlayMaps = {
-    "<span class=\"controles\">Plan d'expropriation</span>": faubourgLayer,
-    "<span class=\"controles\"></span>": geoJSON,
-  },
-
-  /**
-   * Overlay Map
-   * @deprecated
-   */
-  imageUrl = "img/plan-fond-transparent-min.png",
-  imageBounds = [
-    [45.5155088875666, -73.55432183482827],
-    [45.52127103906887, -73.54793549518355]
-  ],
-  plan = L.imageOverlay(imageUrl, imageBounds, {
-    opacity: 1,
-    alt: "Plan d'expropriation du Faubourg à M'lasse"
-  }),
-
-  /** Instantiation du plan */
-  planDuFaubourg = L.map("map", {
-    center: [45.51823567357893, -73.55085910368373],
-    zoom: 17.5,
-    minZoom: 15,
-    zoomDelta: 0.5,
-    layers: [
-      baseLayer,
-      faubourgLayer
-    ]
-  });
-
-  L.control.layers(null, overlayMaps).addTo(planDuFaubourg);
-  geoJSON.addTo(planDuFaubourg);
+  console.log(planDuFaubourg.getPanes());
 
 })();
